c844502fd9282708e4034105b66a2a4b1632e538
Johan Jatko <johan@re3-labs.net>
2017-01-21 19:28:29 +0100

    working basic server

--- server.js
+++ server.js
@@ -1,5 +1,15 @@
-var io = require('socket.io')();
+var http = require('http');
+
+function handler(req, res) {
+  res.writeHead(200);
+  res.end(data);
+}
+var app = http.createServer(handler);
+var io = require('socket.io')(app);
+console.log("starting server");
 
 io.on('connection', function(socket) {
   console.log("connected!");
 });
+
+app.listen(3000);
