d56686c7394d19dc03f2d88735966dcb95eaa995
Johan Jatko <johan@re3-labs.net>
2017-01-30 10:40:37 +0100

    network data object on server-side

--- server.js
+++ server.js
@@ -1,9 +1,25 @@
 var http = require('http');
+var NetworkDataObject = require('./network_data_object').NetworkDataObject;
 
 var io = require('socket.io')
 var socket = io.listen(3001);
 console.log("starting server");
 
 socket.on('connection', function(socket) {
-  console.log("connected!");
+  console.log('connected');
+  socket.on('message', function(data) {
+    console.log(data);
+  });
+  NetworkDataObject.define(socket, 'Task', {
+    get: function(id, callback) {
+      console.log('get Task by id ' + id);
+      callback({
+        'Id': id,
+        'Title': 'Name: ' + id
+      });
+    },
+    list: function(d, callback) {
+      callback(['test']);
+    }
+  })
 });
