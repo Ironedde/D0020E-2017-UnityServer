842a6988d8cca972169d5df8a97fb0bffccd1efe
Johan Jatko <johan@re3-labs.net>
2017-02-02 13:25:15 +0100

    changes to NDO

--- server.js
+++ server.js
@@ -1,25 +1,36 @@
 var http = require('http');
-var NetworkDataObject = require('./network_data_object').NetworkDataObject;
+var ndo = require('./network_data_object').NetworkDataObject;
 
-var io = require('socket.io')
-var socket = io.listen(3001);
+var io = require('socket.io').listen(3001);
 console.log("starting server");
 
-socket.on('connection', function(socket) {
-  console.log('connected');
-  socket.on('message', function(data) {
-    console.log(data);
-  });
-  NetworkDataObject.define(socket, 'Task', {
-    get: function(id, callback) {
-      console.log('get Task by id ' + id);
-      callback({
-        'Id': id,
-        'Title': 'Name: ' + id
-      });
-    },
-    list: function(d, callback) {
-      callback(['test']);
-    }
-  })
+
+
+var Task = ndo.define(io, 'Task', {
+  get: function(id, callback) {
+    Task.subscribe(this, id);
+    console.log('get Task by id ' + id);
+    callback({
+      'Id': id,
+      'Title': 'Name: ' + id
+    });
+  },
+  list: function(d, callback) {
+    console.log("sending");
+    callback(['test']);
+  }
+});
+var Person = ndo.define(io, 'Person', {
+  get: function(id, callback) {
+    Person.subscribe(this, id);
+    callback({
+      'Id': id,
+      'Name': 'Swag ' + id
+    })
+  }
+});
+
+io.on('connection', function(socket) {
+  Task.serve(socket);
+  Person.serve(socket);
 });
